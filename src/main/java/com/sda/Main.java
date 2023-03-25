c5b31d952239e5a17d0b56f1b4c457109f29a28b
GasirP <gasir.pawel@gmail.com>
2023-03-25 10:32:39 +0100

    Fixwd checkstyle

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -2,7 +2,7 @@ package com.sda;
 
 public class Main {
     public static void main(String[] args) {
-        String Test ="test";
-        System.out.println(Test);
+        String test = "test";
+        System.out.println(test);
     }
 }
